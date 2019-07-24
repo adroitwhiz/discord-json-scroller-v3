047bdcb9c595ee81ba9dd403ee952a00849bff59
adroitwhiz <adroitwhiz@protonmail.com>
2019-07-24 06:12:47 -0400

    Use Vue for channel list
    Vueify message rendering
    
    Reduce global state
    
    Update vendored JS modules
    
    misc fixes
    
    Improve style
    
    Vueify main app
    
    Remove custom element bindings
    
    attempt to improve performance
    
    fix wide boi
    
    Add search functionality
    
    fix v-bind key
    
    add find-controls border
    
    nti
    
    nit
    
    nit2
    
    remove debug logging
    
    nit

--- js/main.js
+++ js/main.js
@@ -1,153 +1,156 @@
-const elemIds = {
-	"messageArea":"messages",
-	"channelArea":"channel-list",
-	"renderButton":"render-btn",
-	"messageRenderRangeMin":"msg-range-min",
-	"messageRenderRangeMax":"msg-range-max",
-	"messageJumpNumber":"msg-num",
-	"messageJumpContext":"msg-context",
-	"jumpButton":"jump-btn",
-	"findFilterTextCheckbox":"find-filter-by-text",
-	"findFilterText":"find-filter-text",
-	"findFilterUsernameCheckbox":"find-filter-by-username",
-	"findFilterUsername":"find-filter-username",
-	"findButton":"find-btn",
-	"messageTemplate":"message-template",
-	"channelTemplate":"channel-template",
-	"JSONFilePicker":"json-file-picker"
-},
-      elems = {};
+const readJSONFromFile = file => {
+	return new Promise((resolve, reject) => {
+		const reader = new FileReader();
+		reader.addEventListener('load', event => {
+			resolve(JSON.parse(event.target.result));
+		});
 
-let loadedServer = null;
-let loadedUsers = null;
+		reader.readAsText(file);
+	});
+}
 
-let activeChannel = null;
-let channelMessages = null;
+// Convert URLs into clickable links via Linkify.
+const vueLinkify = (element, binding) => {
+	element = linkifyElement(element, binding.value);
+}
+Vue.directive('linkified', vueLinkify);
 
-function jump(array, n, ctx) {return array.slice(Math.max(0, n-ctx), n+ctx+1)};
+document.addEventListener('DOMContentLoaded', event => {
+	const app = new Vue({
+		el:'#app',
+		data:{
+			server: {channels: [], members: []},
+			messages: [],
+			foundMessages: [],
+			activeChannelId: null,
+			navigationData: {
+				renderRangeMin: 1,
+				renderRangeMax: 10,
+				messageJumpId: '',
+				messageJumpContextAmount: 1,
+			},
+			searchFilterData: {
+				filterByText: false,
+				filterByUser: false,
+				filterByChannel: false,
+				textFilter: '',
+				userFilter: '',
+				channelFilter: ''
+			},
+			findPanelVisible: false
+		},
+		methods: {
+			setChannel (channelId) {
+				activeChannel = this.server.channels[channelId];
+				this.activeChannelId = channelId;
+			},
+			formatMessages (messages) {
+				return messages.reverse().map(message => {
+					return {
+						text:message.content,
+						attachments:message.attachments,
+						username:this.server.members.hasOwnProperty(message.author) ? this.server.members[message.author].username : `<@${message.author}>`,
+						timestamp:moment(parseInt(message.createdTimestamp)).format('MMM D Y h:mm:ss A'),
+						isEdited:message.editedTimestamp !== null,
+						index:message.id
+					}
+				});
+			},
+			displayMessages (messages) {
+				formattedMessages = this.formatMessages(messages);
+				Object.freeze(formattedMessages);
+				this.messages = formattedMessages;
+			},
+			renderMessages () {
+				const currentChannel = this.server.channels[this.activeChannelId];
+				const messagesToRender = currentChannel.messages.slice(
+					parseInt(this.navigationData.renderRangeMin),
+					parseInt(this.navigationData.renderRangeMax + 1)
+				);
 
-document.addEventListener("DOMContentLoaded", event => {
-	const elems = {};
+				this.displayMessages(messagesToRender);
+			},
+			jumpMessages () {
+				const jumpID = this.navigationData.messageJumpId;
+
+				let messageIndex;
+				let messageChannel;
+				for (const channel of Object.values(this.server.channels)) {
+					messageIndex = channel.messages.findIndex(message => message.id === jumpID);
+					if (messageIndex !== -1) {
+						messageChannel = channel;
+						break;
+					}
+				}
+		
+				if (messageIndex === -1) return;
+		
+				let messageJumpContext = this.navigationData.messageJumpContextAmount;
+				let messagesToRender = messageChannel.messages.slice(Math.max(0, messageIndex-messageJumpContext), messageIndex+messageJumpContext+1);
+
+				this.setChannel(messageChannel.id);
+				this.displayMessages(messagesToRender);
+			},
+			searchMessages () {
+				const MAX_MESSAGES = 10000;
+				let allMessages = [];
+				for (const channel of Object.values(this.server.channels)) {
+					if (this.searchFilterData.filterByChannel && this.searchFilterData.channelFilter !== channel.id) continue;
+					let messages = channel.messages;
+
+					if (this.searchFilterData.filterByText) {
+						const matchText = this.searchFilterData.textFilter.toLowerCase();
+						messages = messages.filter(message => message.content.toLowerCase().includes(matchText));
+					}
 	
-	for (let i in elemIds) {
-		if (elemIds.hasOwnProperty(i)) {
-			let elem = document.getElementById(elemIds[i]);
-			if (elem === null) throw new Error(`Could not find element with ID ${elemIds[i]}`);
-			elems[i] = elem;
+					if (this.searchFilterData.filterByUser) {
+						messages = messages.filter(message => message.author === userFilter || this.server.members[message.author].username === userFilter);
+					}
+
+					allMessages = allMessages.concat(messages);
+				}
+
+				if (allMessages.length > MAX_MESSAGES) {
+					alert(`More than ${MAX_MESSAGES} messages found. Not displaying results.`);
+					return;
+				}
+
+				this.foundMessages = this.formatMessages(allMessages).sort((a, b) => a.timestamp - b.timestamp);
+			},
+			loadJSONFromFile (event) {
+				const jsonFile = event.target.files[0];
+				if (jsonFile) {
+					readJSONFromFile(jsonFile).then(this.loadServer);
+				}
+			},
+			loadServer (serializedServer) {
+				const channels = {};
+				for (const channel of serializedServer.channels) {
+					// Messages are read-only. Freezing them stops Vue from trying to "observe" every single message in every single channel.
+					Object.freeze(channel.messages);
+					channels[channel.id] = channel;
+				}
+
+				const members = {};
+				for (const member of serializedServer.members) {
+					members[member.user.id] = member.user;
+				}
+
+				const server = {
+					channels: channels,
+					members: members
+				};
+
+				this.server = server;
+			}
 		}
-	}
-	
-	window.messageTemplate = Handlebars.compile(elems.messageTemplate.innerText);
-	window.channelTemplate = Handlebars.compile(elems.channelTemplate.innerText);
-	
-	//window.mt = messageTemplate;
-	
-	elems.JSONFilePicker.addEventListener("change", event => {
-		console.log(event.target.files[0]);
-		
-		if (event.target.files[0]) {
-			var reader = new FileReader();
-			reader.onload = event => {
-				loadedServer = JSON.parse(event.target.result);
-				return reinit();
-				
-			};
-			
-			reader.readAsText(event.target.files[0]);
-		}
 	});
-	
-	elems.renderButton.addEventListener("click", event => {
-		let messagesToRender = channelMessages.slice(parseInt(elems.messageRenderRangeMin.value), parseInt(elems.messageRenderRangeMax.value));
-		
-		renderMessages(messagesToRender, messageTemplate, elems.messageArea);
+
+	Vue.component('message', {
+		props: ['message'],
+		data: () => {
+			return {}
+		},
+		template: '#message-template'
 	});
-	
-	elems.jumpButton.addEventListener("click", event => {
-		let jumpID = elems.messageJumpNumber.value;
-		
-		let messageToJumpTo = channelMessages.findIndex(message => message.id === jumpID);
-		let messageJumpContext = parseInt(elems.messageJumpContext.value);
-		
-		let messagesToRender = channelMessages.slice(Math.max(0, messageToJumpTo-messageJumpContext), messageToJumpTo+messageJumpContext+1);
-		
-		renderMessages(messagesToRender, elems.messageArea);
-	});
-	
-	//elems.findButton
 });
-
-function reinit() {
-	loadedUsers = {};
-	for (let i = 0; i < loadedServer.members.length; i++) {
-		let user = loadedServer.members[i].user;
-		
-		loadedUsers[user.id] = user;
-	}
-	
-	renderChannels(loadedServer.channels, elems.channelArea);
-}
-
-function renderChannels(channels, channelArea) {
-	for (let i = 0; i < loadedServer.channels.length; i++) {
-		const createdElement = document.createElement(null);
-		
-		createdElement.innerHTML = channelTemplate({
-			channelName:loadedServer.channels[i].name
-		});
-		
-		createdElement.addEventListener("click", event => {
-			setActiveChannel(loadedServer.channels[i].name);
-		});
-		
-		createdElement.setAttribute("data-channel", loadedServer.channels[i].name);
-		
-		elems.channelArea.appendChild(createdElement);
-	}
-}
-
-function setActiveChannel(channelName) {
-	activeChannel = channelName;
-	
-	channelMessages = loadedServer.channels.find(channel => channel.name === channelName).messages;
-	
-	const channelElements = elems.channelArea.children;
-	
-	for (let i = 0; i < channelElements.length; i++) {
-		channelElements[i].children[0].classList.remove("selected");
-	}
-	
-	let activeChannelButton = Array.prototype.find.call(channelElements, (elem => elem.getAttribute("data-channel") === channelName));
-	
-	console.log(activeChannelButton);
-	
-	activeChannelButton.children[0].classList.add("selected");
-}
-
-function renderMessages(messages, messageArea) {
-	const renderedMessages = [];
-	
-	for (let i = 0, len = messages.length; i < len; i++) {
-		renderedMessages.push(renderMessage(messageTemplate, messages[i]));
-	}
-	
-	renderedMessages.reverse();
-	
-	elems.messageArea.innerHTML = renderedMessages.join("");
-	
-	Array.from(elems.messageArea.getElementsByClassName("message-text")).forEach(messageText => {
-		linkifyElement(messageText);
-	});
-}
-
-function renderMessage(template, message) {
-	return template({
-		text:message.content,
-		attachments:message.attachments,
-		username:loadedUsers[message.author] ? loadedUsers[message.author].username : `<@${message.author}>`,
-		timestamp:moment(parseInt(message.createdTimestamp)).format("MMM D Y h:mm:ss A"),
-		isEdited:message.editedTimestamp !== null,
-		index:message.id
-	});
-}
-
