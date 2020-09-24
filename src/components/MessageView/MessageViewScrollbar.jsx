0500c321306c24281aeae50fc94046a95724f83e
adroitwhiz <adroitwhiz@protonmail.com>
2020-09-24 10:06:17 -0400

    fix scrollbar input handling

--- src/components/MessageView/MessageViewScrollbar.jsx
+++ src/components/MessageView/MessageViewScrollbar.jsx
@@ -2,6 +2,35 @@ import style from './style';
 
 import {Component, createRef} from 'preact';
 
+/**
+ * React ****s up text inputs by making onChange fire on *every* input. We only want it to fire when the user confirms
+ * their input, either by unfocusing/blurring the input or by pressing the enter key.
+ */
+class BetterTextInput extends Component {
+	constructor (props) {
+		super(props);
+		this.handleKeyPress = this.handleKeyPress.bind(this);
+	}
+
+	handleKeyPress (event) {
+		if (event.code === 'Enter' || event.code === 'NumpadEnter') {
+			this.props.onChange(event);
+		}
+	}
+
+	render () {
+		return (
+			<input
+				className={this.props.className}
+				type="text"
+				value={this.props.value}
+				onBlur={this.props.onChange}
+				onKeyPress={this.handleKeyPress}
+			/>
+		);
+	}
+}
+
 class MessageViewScrollbar extends Component {
 	constructor (props) {
 		super(props);
@@ -33,9 +62,8 @@ class MessageViewScrollbar extends Component {
 						style={`top: ${(this.props.start / this.props.totalMessages) * 100}%;`}
 					>
 						<div className={`${style['scrollbar-range-top']} ${style['scrollbar-range-indicator']}`}>
-							<input
+							<BetterTextInput
 								className={style['scrollbar-range-input']}
-								type="text"
 								value={this.props.start}
 								onChange={this.handleSetStart}
 							/>
@@ -47,9 +75,8 @@ class MessageViewScrollbar extends Component {
 						style={`top: ${(this.props.end / this.props.totalMessages) * 100}%;`}
 					>
 						<div className={`${style['scrollbar-range-bottom']} ${style['scrollbar-range-indicator']}`}>
-							<input
+							<BetterTextInput
 								className={style['scrollbar-range-input']}
-								type="text"
 								value={this.props.end}
 								onChange={this.handleSetEnd}
 							/>
