53c5583fafceac3ed420deaa84a061aadbce26bd
adroitwhiz <adroitwhiz@protonmail.com>
2021-05-05 09:06:19 -0400

    Break up message runs an hour or more apart

--- src/util/should-split-messages.js
+++ src/util/should-split-messages.js
@@ -1,5 +1,9 @@
 const shouldSplitMessages = (first, second) => {
-	return first.authorID !== second.authorID || second.referencedMessage !== null;
+	return (
+		first.authorID !== second.authorID ||
+		second.referencedMessage !== null ||
+		Math.abs(second.createdTimestamp - first.createdTimestamp) >= (60 * 60 * 1000)
+	);
 };
 
 export default shouldSplitMessages;
